d83ac323982f42eedbdf92bd50705941eb182fe2
maxm24 <709003@student.dusd.net>
2022-11-29 15:24:17 -0800

    fixed colliders
    fixed my prefabs and colliders

--- RubysAdventureMiramontesP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureMiramontesP7/Assets/Scripts/RubyController.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //create variables to use the built in axis
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
+    private void FixedUpdate()
+    {
         //create a movement vector
         Vector2 position = transform.position;
 
         //make horizontal and vertical movement work
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        
-        transform.position = position;
+
+        rigidbody2d.MovePosition(position);
     }
 }
